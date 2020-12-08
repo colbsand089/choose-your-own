b5e9c04d398fa7ab0b83a90f321138566065c861
colbey sands <colbsand089@outlook.com>
2020-12-08 14:24:01 -0500

    Add project files.

---
+++ choose your own/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace choose_your_own
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.greenButtonOutputLabel = new System.Windows.Forms.Label();
+            this.redButtonOutputLabel = new System.Windows.Forms.Label();
+            this.greenLabel = new System.Windows.Forms.Label();
+            this.redLabel = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("mono 07_55", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.outputLabel.ForeColor = System.Drawing.Color.Green;
+            this.outputLabel.Location = new System.Drawing.Point(3, 9);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(366, 102);
+            this.outputLabel.TabIndex = 0;
+            this.outputLabel.Text = "you are trapped in a resturaunt with Gordon Ramsey ";
+            // 
+            // greenButtonOutputLabel
+            // 
+            this.greenButtonOutputLabel.AutoSize = true;
+            this.greenButtonOutputLabel.ForeColor = System.Drawing.Color.Green;
+            this.greenButtonOutputLabel.Location = new System.Drawing.Point(83, 319);
+            this.greenButtonOutputLabel.Name = "greenButtonOutputLabel";
+            this.greenButtonOutputLabel.Size = new System.Drawing.Size(26, 13);
+            this.greenButtonOutputLabel.TabIndex = 3;
+            this.greenButtonOutputLabel.Text = "play";
+            // 
+            // redButtonOutputLabel
+            // 
+            this.redButtonOutputLabel.AutoSize = true;
+            this.redButtonOutputLabel.ForeColor = System.Drawing.Color.Red;
+            this.redButtonOutputLabel.Location = new System.Drawing.Point(83, 356);
+            this.redButtonOutputLabel.Name = "redButtonOutputLabel";
+            this.redButtonOutputLabel.Size = new System.Drawing.Size(0, 13);
+            this.redButtonOutputLabel.TabIndex = 4;
+            // 
+            // greenLabel
+            // 
+            this.greenLabel.BackColor = System.Drawing.Color.Green;
+            this.greenLabel.Location = new System.Drawing.Point(29, 308);
+            this.greenLabel.Name = "greenLabel";
+            this.greenLabel.Size = new System.Drawing.Size(27, 24);
+            this.greenLabel.TabIndex = 5;
+            // 
+            // redLabel
+            // 
+            this.redLabel.BackColor = System.Drawing.Color.Red;
+            this.redLabel.Location = new System.Drawing.Point(29, 356);
+            this.redLabel.Name = "redLabel";
+            this.redLabel.Size = new System.Drawing.Size(27, 24);
+            this.redLabel.TabIndex = 6;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.pictureBox1.Location = new System.Drawing.Point(7, 114);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(370, 161);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(381, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.redLabel);
+            this.Controls.Add(this.greenLabel);
+            this.Controls.Add(this.redButtonOutputLabel);
+            this.Controls.Add(this.greenButtonOutputLabel);
+            this.Controls.Add(this.outputLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label greenButtonOutputLabel;
+        private System.Windows.Forms.Label redButtonOutputLabel;
+        private System.Windows.Forms.Label greenLabel;
+        private System.Windows.Forms.Label redLabel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ choose your own/Form1.cs
@@ -0,0 +1,345 @@
+﻿// colbey sands
+// december 8 
+// choose your path adventure game
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace choose_your_own
+{
+    public partial class Form1 : Form
+    {
+        int scene = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.G)       //greem button press
+            {
+                if (scene == 0)
+                {
+                    scene = 1;
+                }
+                else if (scene == 1)
+                {
+                    scene = 4;
+                }
+                else if (scene == 2)
+                {
+                    scene = 4;
+                }
+                else if (scene == 3)
+                {
+                    scene = 0;
+                }
+                else if (scene == 4)
+                {
+                    scene = 6;
+                }
+                else if (scene == 5)
+                {
+                    scene = 0;
+                }
+                else if (scene == 6)
+                {
+                    scene = 8;
+                }
+                else if (scene == 7)
+                {
+                    scene = 0;
+                }
+                else if (scene == 8)
+                {
+                    scene = 10;
+                }
+                else if (scene == 9)
+                {
+                    scene = 0;
+                }
+                else if (scene == 10)
+                {
+                    scene = 12;
+                }
+                else if (scene == 11)
+                {
+                    scene = 0;
+                }
+                else if (scene == 12)
+                {
+                    scene = 14;
+                }
+                else if (scene == 13)
+                {
+                    scene = 0;
+                }
+                else if (scene == 14)
+                {
+                    scene = 16;
+                }
+                else if (scene == 15)
+                {
+                    scene = 0;
+                }
+                else if (scene == 16)
+                {
+                    scene = 18;
+                }
+                else if (scene == 17)
+                {
+                    scene = 0;
+                }
+                else if (scene == 18)
+                {
+                    scene = 0;
+                }
+               
+            }
+            else if (e.KeyCode == Keys.R)  //Red button press
+            {
+                if (scene == 0)
+                {
+                    scene = 1;
+                }
+                else if (scene == 1)
+                {
+                    scene = 2;
+                }
+                else if (scene == 2)
+                {
+                    scene = 3;
+                }
+                else if (scene == 3)
+                {
+                    scene = 0;
+                }
+                else if (scene == 4)
+                {
+                    scene = 5;
+                }
+                else if (scene == 5)
+                {
+                    scene = 0;
+                }
+                else if (scene == 6)
+                {
+                    scene = 7;
+                }
+                else if (scene == 7)
+                {
+                    scene = 0;
+                }
+                else if (scene == 8)
+                {
+                    scene = 9;
+                }
+                else if (scene == 9)
+                {
+                    scene = 0;
+                }
+                else if (scene == 10)
+                {
+                    scene = 11;
+                }
+                else if (scene == 11)
+                {
+                    scene = 0;
+                }
+                else if (scene == 12)
+                {
+                    scene = 13;
+                }
+                else if (scene == 13)
+                {
+                    scene = 0;
+                }
+                else if (scene == 14)
+                {
+                    scene = 15;
+                }
+                else if (scene == 15)
+                {
+                    scene = 0;
+                }
+                else if (scene == 16)
+                {
+                    scene = 17;
+                }
+                else if (scene == 17)
+                {
+                    scene = 0;
+                }
+                else if (scene == 18)
+                {
+                    scene = 0;
+                }
+            }
+
+
+            /// the scenes and the conent in them
+            switch (scene)
+            {
+                case 0:  //start scene  
+                    pictureBox1.BackgroundImage = Properties.Resources.gordan;
+
+                    outputLabel.Text = "You are trapped with Gordon Ramsey in a restaurant";
+                    greenButtonOutputLabel.Text = "Play";
+                    redButtonOutputLabel.Text = "";
+                    break;
+
+                case 1:
+                    pictureBox1.BackgroundImage = Properties.Resources.gordonWaiting;
+
+                    outputLabel.Text = "You are trapped with Gordon Ramsey in a restaurant. He askes you to cook him a meal";
+                    greenButtonOutputLabel.Text = "Cook";
+                    redButtonOutputLabel.Text = "Dont cook";
+                    break;
+
+                case 2:
+                    outputLabel.Text = "He gives you one more chance to cook him a meal";
+                    greenButtonOutputLabel.Text = "Cook";
+                    redButtonOutputLabel.Text = "Dont cook";
+                    break;
+
+                case 3:
+                    pictureBox1.BackgroundImage = Properties.Resources.insideOven;
+
+                    outputLabel.Text = "He stuffs you in an oven and cooks you";
+                    greenButtonOutputLabel.Text = "play again?";
+                    redButtonOutputLabel.Text = "dont play again";
+                    break;
+
+                case 4:
+                    pictureBox1.BackgroundImage = Properties.Resources.gordonTalking;
+
+                    outputLabel.Text = "he tells you to make him eggs and bacon";
+                    greenButtonOutputLabel.Text = "Make crispy bacon";
+                    redButtonOutputLabel.Text = "Make chewy bacon";
+                    break;
+
+                case 5:
+                    pictureBox1.BackgroundImage = Properties.Resources.madgordan;
+
+                    outputLabel.Text = "he hates chewy bacon and and strangles you with his shoelace";
+                    greenButtonOutputLabel.Text = "Play again?";
+                    redButtonOutputLabel.Text = "Dont play again";
+                    break;
+                case 6:
+                    pictureBox1.BackgroundImage = Properties.Resources.happy;
+
+                    outputLabel.Text = "gordon likes the crispy bacon and asks for a drink with it";
+                    greenButtonOutputLabel.Text = "Water";
+                    redButtonOutputLabel.Text = "Juice";
+                    break;
+
+                case 7:
+                    pictureBox1.BackgroundImage = Properties.Resources.mad_2_0;
+
+                    outputLabel.Text = "gordon does not like the juice you brought him and he drowns you in it";
+                    greenButtonOutputLabel.Text = "Play again?";
+                    redButtonOutputLabel.Text = "dont Play again";
+                    break;
+
+                case 8:
+                    pictureBox1.BackgroundImage = Properties.Resources.gordan;
+
+                    outputLabel.Text = "he drinks the water and gives you his plate he then asks for desert";
+                    greenButtonOutputLabel.Text = "Give him lamb sauce";
+                    redButtonOutputLabel.Text = "Give him ice cream";
+                    break;
+
+                case 9:
+                    pictureBox1.BackgroundImage = Properties.Resources.lamb;
+
+                    outputLabel.Text = "he takes the ice cream and throws it across the room then throws you across the room";
+                    greenButtonOutputLabel.Text = "Play again";
+                    redButtonOutputLabel.Text = "Dont play again";
+                    break;
+
+                case 10:
+                    pictureBox1.BackgroundImage = Properties.Resources.happy;
+
+                    outputLabel.Text = "he loves the lamb sauce its his favorite, he needs you to clean up.";
+                    greenButtonOutputLabel.Text = "Grab the plates";
+                    redButtonOutputLabel.Text = "Grab the cups";
+                    break;
+
+                case 11:
+                    pictureBox1.BackgroundImage = Properties.Resources.Knifes;
+
+                    outputLabel.Text = "the cups are wet and you drop one throwing gordon into a rage he stabs you";
+                    greenButtonOutputLabel.Text = "Play again";
+                    redButtonOutputLabel.Text = "Dont play again";
+                    break;
+
+                case 12:
+                    pictureBox1.BackgroundImage = Properties.Resources.money;
+
+                    outputLabel.Text = "you take the plate to the sink. he gives you $400000 and askes you to get him something";
+                    greenButtonOutputLabel.Text = "Get him a knife";
+                    redButtonOutputLabel.Text = "Get him a spoon";
+                    break;
+
+                case 13:
+                    pictureBox1.BackgroundImage = Properties.Resources.spoon;
+
+                    outputLabel.Text = "he scoops out your eye with the horrible spoon you gave him";
+                    greenButtonOutputLabel.Text = "Play agian?";
+                    redButtonOutputLabel.Text = "Dont play again";
+                    break;
+
+                case 14:
+                    pictureBox1.BackgroundImage = Properties.Resources.knool;
+
+                    outputLabel.Text = "gordan loves knives and wants to show you his skills";
+                    greenButtonOutputLabel.Text = "yeah sure";
+                    redButtonOutputLabel.Text = "Not now";
+                    break;
+
+                case 15:
+                    pictureBox1.BackgroundImage = Properties.Resources.bleeeding;
+
+                    outputLabel.Text = "he cuts you up with the knife";
+                    greenButtonOutputLabel.Text = "Play again?";
+                    redButtonOutputLabel.Text = "Dont play again";
+                    break;
+
+                case 16:
+                    pictureBox1.BackgroundImage = Properties.Resources.steak;
+
+                    outputLabel.Text = "he cuts up some steak and gives it to you, it tastes bad";
+                    greenButtonOutputLabel.Text = "pretend its good";
+                    redButtonOutputLabel.Text = "tell him its bad";
+                    break;
+
+                case 17:
+                    pictureBox1.BackgroundImage = Properties.Resources.W;
+
+                    outputLabel.Text = "he lets you go because he likes you, you win";
+                    greenButtonOutputLabel.Text = "play again?";
+                    redButtonOutputLabel.Text = "";
+                    break;
+
+                case 18:
+                    pictureBox1.BackgroundImage = Properties.Resources.lose;
+
+                    outputLabel.Text = "he calls you a donkey and hits you with a chair, you lose ";
+                    greenButtonOutputLabel.Text = "Play again?";
+                    redButtonOutputLabel.Text = "";
+                    break;
+                default:
+                    break;
+            }
+        }
+
+       
+    }
+}
